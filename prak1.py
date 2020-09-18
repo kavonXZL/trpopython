d30f7a0dd3a228ed4a087bb59d319c1ae9fdb429
Aisen Shadrin <xeezl666@mail.ru>
2020-09-18 12:04:10 +0900

    commit

---
+++ prak1.py
@@ -0,0 +1,19 @@
+#Задание 29
+num = int(input("Введите число: "))
+a = num
+a = a * a
+a = a * a
+print("Ответ а) ", a)
+a = num
+a = a * a
+b = a * a
+a = a * b
+print("Ответ б) ", a)
+a = num
+b = a * a
+c = b * a
+d = c * c
+e = d * d
+a = c * e
+print("Ответ в) ", a)
+print("git #2")
